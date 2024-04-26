aa0d0972362a94cfac84af54472f33bef3e787c2
Prashasti Karlekar <prashastikarlekar@gmail.com>
2024-04-26 17:30:53 -0500

    Appwrite setup completed, Sign in & Sign up completed.

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,17 +1,37 @@
 /** @format */
 
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { Link } from "expo-router";
+import { getCurrentUser, signIn } from "../../lib/appwrite";
 
 const SignIn = () => {
 	const [form, setForm] = useState({ email: "", password: "" });
 	const [isSubmitting, setIsSubmitting] = useState(false);
-	const submit = () => {};
+	const submit = async () => {
+		if (!form.email || !form.password) {
+			Alert.alert("Error", "Please fill all the fields");
+		}
+		setIsSubmitting(true);
+
+		try {
+			await signIn(form.email, form.password);
+			const result = await getCurrentUser();
+			setSourceMapRange(result);
+			setIsLoggedIn(true);
+
+			Alert.alert("Success", "Logged in successfully");
+			router.replace("/home");
+		} catch (error) {
+			Alert.alert("Error", error.message);
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
 	return (
 		<SafeAreaView className='bg-primary h-full'>
 			<ScrollView>
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,17 +1,34 @@
 /** @format */
 
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
 	const [form, setForm] = useState({ username: "", email: "", password: "" });
 	const [isSubmitting, setIsSubmitting] = useState(false);
-	const submit = () => {};
+	const submit = async () => {
+		if (!form.username || !form.email || !form.password) {
+			Alert.alert("Error", "Please fill all the fields");
+		}
+		setIsSubmitting(true);
+
+		try {
+			const result = await createUser(form.email, form.password, form.username);
+			setUser(result);
+			setIsLoggedIn(true);
+			router.replace("/home");
+		} catch (error) {
+			Alert.alert("Error", error.message);
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
 	return (
 		<SafeAreaView className='bg-primary h-full'>
 			<ScrollView>
